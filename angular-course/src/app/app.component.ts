209c51c6a5a0614ba76040d3f47b8d4de2722483
Lina Sachuk <sachuklina@gmail.com>
2021-02-21 20:44:26 -0700

    add data binding

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Core Deep Dive';
+
+  data = {
+    title :'Angular Core Deep Dive'
+  }
+
+onLogoClicked(){
+  alert('Hello World!')
+}
+
+onKeyUp(newTitle: string){
+  this.data.title = newTitle;
+}
+
 }
